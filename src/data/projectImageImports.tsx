1e5e97a30f7b20f3f24d9a37df753da315978e6a
Laura Leesi <51978253+lauraleesi@users.noreply.github.com>
2025-08-20 00:37:51 +1000

    build for deploy

--- src/data/projectImageImports.tsx
+++ src/data/projectImageImports.tsx
@@ -2,11 +2,6 @@
 import ggjLogo from "../images/GGJ00_Logo_Dark.svg";
 import brisbanePowerhouseLogo from "../images/BrisbanePowerhouseLogo.svg";
 
-export { 
-    ggjLogo,
-    brisbanePowerhouseLogo,
-};
-
 // Blossom project images
 import blossomFeatureImage from "../images/Blossom/blossomfeature.png_itok=j0LdQOrF&timestamp=1580623155.png";
 import blossomProgressImage from "../images/Blossom/treegrowth.png_itok=ac4oL9P2&timestamp=1580623155.png";
@@ -15,49 +10,21 @@ import blossomGrowthStagesImage from "../images/Blossom/inprogressplaysession.pn
 import blossomStartImage from "../images/Blossom/deadstill.png_itok=N9YJl1pT&timestamp=1580623155.png";
 import blossomHealedImage from "../images/Blossom/levelfinish_0.png_itok=hruWEam3&timestamp=1580639115.png";
 
-export { 
-    blossomFeatureImage,
-    blossomProgressImage,
-    blossomEffectImage,
-    blossomGrowthStagesImage,
-    blossomStartImage,
-    blossomHealedImage
-};
-
 // Briefcase Mystery Demo project images
-import briefcaseMysteryDemoFeatureImage from "../images/BriefcaseMysteryDemo/BriefcaseMysteryDemofeature.png_itok=j0LdQOrF&timestamp=1580623155.png";
-import briefcaseMysteryDemoStartRoomImage from "../images/BriefcaseMysteryDemo/treegrowth.png_itok=ac4oL9P2&timestamp=1580623155.png";
-import briefcaseMysteryDemoBriefcaseImage from "../images/BriefcaseMysteryDemo/blue_flower.png_itok=5JBrD0-j&timestamp=1580623155.png";
-import briefcaseMysteryDemoSimonSaysImage from "../images/BriefcaseMysteryDemo/inprogressplaysession.png_itok=bwpElKhA&timestamp=1580623155.png";
-import briefcaseMysteryDemoRoomOverviewImage from "../images/BriefcaseMysteryDemo/deadstill.png_itok=N9YJl1pT&timestamp=1580623155.png";
-
-export { 
-    briefcaseMysteryDemoFeatureImage,
-    briefcaseMysteryDemoStartRoomImage,
-    briefcaseMysteryDemoBriefcaseImage,
-    briefcaseMysteryDemoSimonSaysImage,
-    briefcaseMysteryDemoRoomOverviewImage,
-};
+import briefcaseMysteryDemoFeatureImage from "../images/BriefcaseMysteryDemo/briefcaseSmall.jpg";
+import briefcaseMysteryDemoStartRoomImage from "../images/BriefcaseMysteryDemo/startingRoom.jpg";
+import briefcaseMysteryDemoBriefcaseImage from "../images/BriefcaseMysteryDemo/briefcase.jpg";
+import briefcaseMysteryDemoSimonSaysImage from "../images/BriefcaseMysteryDemo/simonSays.jpg";
+import briefcaseMysteryDemoRoomOverviewImage from "../images/BriefcaseMysteryDemo/roomOverview.jpg";
 
 // Bumble Bots project images
 import bumbleBotsFeatureImage from "../images/BumbleBots/flight3.jpg";
 import bumbleBotsFlight1Image from "../images/BumbleBots/flight1.jpg";
-import bumbleBotsFlight2Image from "../images/BumbleBots/flight2.jpg";
+import bumbleBotsFlight2Image from "../images/BumbleBots/flight2.png";
 import bumbleBotsLevelImage from "../images/BumbleBots/levelView.png";
-
-export { 
-    bumbleBotsFeatureImage,
-    bumbleBotsFlight1Image,
-    bumbleBotsFlight2Image,
-    bumbleBotsLevelImage,
-};
 
 // Charge Rider project images
 import chargeRiderFeatureImage from "../images/ChargeRider/world.png";
-
-export {
-    chargeRiderFeatureImage,
-};
 
 // Stranded project images
 import strandedFeatureImage from "../images/Stranded/titleImage.png";
@@ -65,14 +32,6 @@ import strandedWaterfallImage from "../images/Stranded/waterfallIsland.png";
 import strandedNPCImage from "../images/Stranded/npcRequest.png";
 import strandedCraftingImage from "../images/Stranded/craftingMenu.png";
 import strandedTradingImage from "../images/Stranded/tradingMenu.png";
-
-export {
-    strandedFeatureImage,
-    strandedWaterfallImage,
-    strandedNPCImage,
-    strandedCraftingImage,
-    strandedTradingImage,
-};
 
 // Wizard Car project images
 import wizardCatFeatureImage from "../images/WizardCat/pls.png";
@@ -82,7 +41,44 @@ import wizardCatMirrorImage from "../images/WizardCat/Bedroom.png";
 import wizardCatRoofImage from "../images/WizardCat/agh.png";
 import wizardCatCandleImage from "../images/WizardCat/unknown.png";
 
+
 export { 
+    // General images
+    ggjLogo,
+    brisbanePowerhouseLogo,
+
+    // Blossom project images
+    blossomFeatureImage,
+    blossomProgressImage,
+    blossomEffectImage,
+    blossomGrowthStagesImage,
+    blossomStartImage,
+    blossomHealedImage,
+
+    // Briefcase Mystery Demo project images
+    briefcaseMysteryDemoFeatureImage,
+    briefcaseMysteryDemoStartRoomImage,
+    briefcaseMysteryDemoBriefcaseImage,
+    briefcaseMysteryDemoSimonSaysImage,
+    briefcaseMysteryDemoRoomOverviewImage,
+
+    // Bumble Bots project images
+    bumbleBotsFeatureImage,
+    bumbleBotsFlight1Image,
+    bumbleBotsFlight2Image,
+    bumbleBotsLevelImage,
+
+    // Charge Rider project images
+    chargeRiderFeatureImage,
+
+    // Stranded project images
+    strandedFeatureImage,
+    strandedWaterfallImage,
+    strandedNPCImage,
+    strandedCraftingImage,
+    strandedTradingImage,
+
+    // Wizard Car project images
     wizardCatFeatureImage,
     wizardCatKitchenImage,
     wizardCatFishImage,
